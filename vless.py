2e41d49949e7379e6a0ac425b2de3565696964e6
shatinz <shatincia2@gmail.com>
2026-04-15 13:54:53 +0000

    Remove sensitive database files from tracking and add to .gitignore

--- vless.py
+++
@@ -1,6 +0,0 @@
-vless_trial = []
-vless_week = []
-vless_month = [
-  "vless://422a7f06-69a8-4fb4-90a4-41c6c30c3c7d@po.masterdadeh.ir:80?type=ws&path=%2F&security=none#trPouyarefill"
-]
-vless_3month = []
